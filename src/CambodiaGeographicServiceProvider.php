cfba814dae467e12aa65029e88fd990863292e27
kriyar <kriyar@ktechzone.com>
2019-07-28 01:07:15 +0700

    Updated service provider

--- src/CambodiaGeographicServiceProvider.php
+++ src/CambodiaGeographicServiceProvider.php
@@ -7,23 +7,13 @@ use Illuminate\Support\ServiceProvider;
 class CambodiaGeographicServiceProvider extends ServiceProvider
 {
   /**
-  * Indicates if loading of the provider is deferred.
-  *
-  * @var bool
-  */
-  protected $defer = false;
-
-  /**
   * Register services.
   *
   * @return void
   */
   public function register()
   {
-    $this->app->make('Kriyar\CambodiaGeographic\CityProvinceApiController');
-    $this->app->make('Kriyar\CambodiaGeographic\DistrictApiController');
-    $this->app->make('Kriyar\CambodiaGeographic\CommuneApiController');
-    $this->app->make('Kriyar\CambodiaGeographic\VillageApiController');
+    $this->loadRoutesFrom(__DIR__.'/routes/api.php');
   }
 
   /**
@@ -33,7 +23,6 @@ class CambodiaGeographicServiceProvider extends ServiceProvider
   */
   public function boot()
   {
-    $this->loadRoutesFrom(__DIR__.'/routes/api.php');
     $this->publishes([__DIR__.'/database/migrations' => database_path('migrations')], 'cambodia-geographic-migrations');
     $this->publishes([__DIR__.'/database/seeds' => database_path('seeds')], 'cambodia-geographic-seeds');
   }
