550c141498eaa8f57ebe26838ad9b0da01744cec
kriyar <kriyar.pro@gmail.com>
2019-07-27 11:45:56 +0700

    Updated tag to publish database and seed to laravel directory

--- src/CambodiaGeographicServiceProvider.php
+++ src/CambodiaGeographicServiceProvider.php
@@ -6,28 +6,35 @@ use Illuminate\Support\ServiceProvider;
 
 class CambodiaGeographicServiceProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->app->make('Kriyar\CambodiaGeographic\Http\Api\CityProvinceApiController');
-        $this->app->make('Kriyar\CambodiaGeographic\Http\Api\DistrictApiController');
-        $this->app->make('Kriyar\CambodiaGeographic\Http\Api\CommuneApiController');
-        $this->app->make('Kriyar\CambodiaGeographic\Http\Api\VillageApiController');
-    }
+  /**
+  * Indicates if loading of the provider is deferred.
+  *
+  * @var bool
+  */
+  protected $defer = false;
 
-    /**
-     * Bootstrap services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-      $this->loadRoutesFrom(__DIR__.'/routes/api.php');
-      $this->publishes([__DIR__.'/database/migrations' => database_path('migrations')], 'migrations');
-      $this->publishes([__DIR__.'/database/seeds' => database_path('seeds')], 'seed');
-    }
+  /**
+  * Register services.
+  *
+  * @return void
+  */
+  public function register()
+  {
+    $this->app->make('Kriyar\CambodiaGeographic\Http\Api\CityProvinceApiController');
+    $this->app->make('Kriyar\CambodiaGeographic\Http\Api\DistrictApiController');
+    $this->app->make('Kriyar\CambodiaGeographic\Http\Api\CommuneApiController');
+    $this->app->make('Kriyar\CambodiaGeographic\Http\Api\VillageApiController');
+  }
+
+  /**
+  * Bootstrap services.
+  *
+  * @return void
+  */
+  public function boot()
+  {
+    $this->loadRoutesFrom(__DIR__.'/routes/api.php');
+    $this->publishes([__DIR__.'/database/migrations' => database_path('migrations')], 'cambodia-geographic-migrations');
+    $this->publishes([__DIR__.'/database/seeds' => database_path('seeds')], 'cambodia-geographic-seed');
+  }
 }
